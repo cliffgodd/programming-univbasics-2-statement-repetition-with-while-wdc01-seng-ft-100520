0053a449e65f92f8f29163b8d1765b02dfb3f8e6
Justin Clifford <justoon7.jc@gmail.com>
2020-08-04 03:41:44 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,8 @@
 # Write your code here
-count = 11
-while count > 0 do
+count = 10
+while count >= 1 do
   puts count
-  if count = 0 puts "Happy New Year"
-    break
+  break if count = 0
+   puts "Happy New Year!"
     count -= 1
 end
